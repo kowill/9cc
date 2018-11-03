fffa18c79c5072994d0e0b2a13d67d7f934c2b91
kowill <mob.koya@gmail.com>
2018-11-03 13:25:42 +0900

    first step

---
+++ test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+try() {
+  expected="$1"
+  input="$2"
+
+  ./9cc "$input" > tmp.s
+  gcc -o tmp tmp.s
+  ./tmp
+  actual="$?"
+
+  if [ "$actual" != "$expected" ]; then
+    echo "$input expected $expected, but got $actual"
+    exit 1
+  fi
+}
+
+try 0 0
+try 192 192
+try 255 255
+
+echo OK
